cbf78b36a6602e21773be92333983c6e633919aa
z1qnezt <misha232dog@gmail.com>
2024-05-19 22:18:31 +0300

    5

--- build/third-party/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mihailerosenko/z1qnezt/workspace/RK2/third-party/gtest
-# Build directory: /Users/mihailerosenko/z1qnezt/workspace/RK2/build/third-party/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/third-party/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mihailerosenko/z1qnezt/workspace/RK2/third-party/gtest/googlemock
-# Build directory: /Users/mihailerosenko/z1qnezt/workspace/RK2/build/third-party/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
