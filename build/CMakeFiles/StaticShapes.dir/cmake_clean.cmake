ff801c985b02b8cfa9125936bd637fbbaf933fff
z1qnezt <misha232dog@gmail.com>
2024-05-19 21:18:26 +0300

    task4

---
+++ build/CMakeFiles/StaticShapes.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StaticShapes.dir/StaticShapes.cpp.o"
+  "CMakeFiles/StaticShapes.dir/StaticShapes.cpp.o.d"
+  "libStaticShapes.a"
+  "libStaticShapes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StaticShapes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
