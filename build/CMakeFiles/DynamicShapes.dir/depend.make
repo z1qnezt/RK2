3298bc0db65147b79ba943e9755e3c153d9a1f73
_Twent <76701971+z1qnezt@users.noreply.github.com>
2024-05-19 22:19:53 +0300

    f

--- build/CMakeFiles/DynamicShapes.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for DynamicShapes.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/Shapes.dir/Program.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Shapes.dir/Program.cpp.o: \
-  /Users/mihailerosenko/z1qnezt/workspace/RK2/Program.cpp
