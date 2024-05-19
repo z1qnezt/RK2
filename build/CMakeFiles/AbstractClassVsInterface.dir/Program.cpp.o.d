aab1a7b1255df0bb0ebb0a8460bc751a06386cd0
z1qnezt <misha232dog@gmail.com>
2024-05-19 20:19:36 +0300

    task2

---
+++ build/CMakeFiles/AbstractClassVsInterface.dir/Program.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/AbstractClassVsInterface.dir/Program.cpp.o: \
+  /Users/mihailerosenko/z1qnezt/workspace/RK2/Program.cpp
---
+++ build/CMakeFiles/AbstractClassVsInterface.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "AbstractClassVsInterface"
+  "AbstractClassVsInterface.pdb"
+  "CMakeFiles/AbstractClassVsInterface.dir/ConceptualExample.cpp.o"
+  "CMakeFiles/AbstractClassVsInterface.dir/ConceptualExample.cpp.o.d"
+  "CMakeFiles/AbstractClassVsInterface.dir/DynamicShapes.cpp.o"
+  "CMakeFiles/AbstractClassVsInterface.dir/DynamicShapes.cpp.o.d"
+  "CMakeFiles/AbstractClassVsInterface.dir/Program.cpp.o"
+  "CMakeFiles/AbstractClassVsInterface.dir/Program.cpp.o.d"
+  "CMakeFiles/AbstractClassVsInterface.dir/StaticShapes.cpp.o"
+  "CMakeFiles/AbstractClassVsInterface.dir/StaticShapes.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AbstractClassVsInterface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
